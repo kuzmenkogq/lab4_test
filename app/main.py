62f6f61f4873e7b890a8283f6fd12d5b6f450f53
Kuzmenko <kuzmenkogeorge1713@gmail.com>
2026-06-07 17:24:50 +0300

    Fix flake8 issues

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from app.password_generator import generate_password
 
 app = Flask(__name__)
 
+
 @app.route("/generate-password", methods=["POST"])
 def generate():
     data = request.get_json()
@@ -12,7 +13,12 @@ def generate():
         use_symbols = bool(data.get("use_symbols", True))
         use_uppercase = bool(data.get("use_uppercase", True))
 
-        password = generate_password(length, use_digits, use_symbols, use_uppercase)
+        password = generate_password(
+            length,
+            use_digits,
+            use_symbols,
+            use_uppercase
+        )
         return jsonify({"password": password})
     except Exception as e:
         return jsonify({"error": str(e)}), 400
